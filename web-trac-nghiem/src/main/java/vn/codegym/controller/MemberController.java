b093bcc4f29177ac4eea0c71a31563293b10493b
trung <trunghoa130299@gmail.com>
2021-08-21 21:35:19 +0700

    trung

---
+++ web-trac-nghiem/src/main/java/vn/codegym/controller/MemberController.java
@@ -0,0 +1,20 @@
+package vn.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+    @GetMapping(value = "/view")
+    public String showMemberView(){
+        return "member/view";
+    }
+    @GetMapping(value = "/editMember")
+    public String showMemberEdit(){
+        return "member/editMember";
+    }
+    @GetMapping(value = "/editPass")
+    public String showMemberEditPass(){
+        return "member/editPass";
+    }
+}
