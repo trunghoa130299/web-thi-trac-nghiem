9c600ad5791d3b7425d095d6ebd42dcd73134005
trung <trunghoa130299@gmail.com>
2021-08-23 14:00:44 +0700

    ok

--- web-trac-nghiem/src/main/java/vn/codegym/controller/HomeController.java
+++ web-trac-nghiem/src/main/java/vn/codegym/controller/HomeController.java
@@ -6,6 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("/index")
+    public String showIndex(){
+        return "index";
+    }
+    @GetMapping("/them")
+    public String showThem(){
+        return "them";
+    }
+
     @GetMapping("/list")
     public String showList(){
         return "list/List_Information";
