0206b3dd2ce4c7ec4503bacf67c0a21b3cf7e42b
trung <trunghoa130299@gmail.com>
2021-08-23 13:42:31 +0700

    ok

--- web-trac-nghiem/src/main/java/vn/codegym/controller/HomeController.java
+++ web-trac-nghiem/src/main/java/vn/codegym/controller/HomeController.java
@@ -1,7 +1,26 @@
 package vn.codegym.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/list")
+    public String showList(){
+        return "list/List_Information";
+    }
+
+    @GetMapping("/addQuestion")
+    public String addQuestion(){
+        return "question/addQS";
+    }
+    @GetMapping("/listExam")
+    public String listExam(){
+        return "question/listExamQuestions";
+    }
+    @GetMapping("/questionList")
+    public String questionList(){
+        return "question/questionList";
+    }
 }
--- web-trac-nghiem/src/main/java/vn/codegym/model/User.java
+++ web-trac-nghiem/src/main/java/vn/codegym/model/User.java
@@ -1,21 +1,21 @@
-package vn.codegym.model;
-
-import javax.persistence.*;
-import java.util.Set;
-
-@Entity
-public class User {
-    @Id
-    private String user;
-    private String passWord;
-    private String rePassWord;
-    private String fullName;
-    private String email;
-    private String address;
-    private String phoneNumber;
-
-    @ManyToMany(cascade = CascadeType.PERSIST,fetch = FetchType.EAGER)
-    @JoinTable(name = "user_role",joinColumns = @JoinColumn(name = "userName", referencedColumnName = "userName"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-    private Set<Role> roles;
-}
+//package vn.codegym.model;
+//
+//import javax.persistence.*;
+//import java.util.Set;
+//
+//@Entity
+//public class User {
+//    @Id
+//    private String user;
+//    private String passWord;
+//    private String rePassWord;
+//    private String fullName;
+//    private String email;
+//    private String address;
+//    private String phoneNumber;
+//
+//    @ManyToMany(cascade = CascadeType.PERSIST,fetch = FetchType.EAGER)
+//    @JoinTable(name = "user_role",joinColumns = @JoinColumn(name = "userName", referencedColumnName = "userName"),
+//            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+//    private Set<Role> roles;
+//}
