4e05b433a4f9f46d4b6c8053b2f0509cd1a18533
trung <trunghoa130299@gmail.com>
2021-08-29 15:48:20 +0700

    29/8

--- web-trac-nghiem/src/main/java/vn/codegym/controller/TrungController.java
+++ web-trac-nghiem/src/main/java/vn/codegym/controller/TrungController.java
@@ -6,12 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import vn.codegym.model.User;
 import vn.codegym.service.UserService;
 
 @Controller
-public class UserController {
+public class TrungController {
 
     @Autowired
     UserService userService;
@@ -29,5 +30,37 @@ public class UserController {
         user.setRePassWord(passwordEncoder.encode(user.getRePassWord()));
         userService.save(user);
         return "login";
+    }
+
+
+    @GetMapping(value = "/view/{userName}")
+    public String showMemberView(@PathVariable String userName, Model model){
+        User user = userService.findByUser(userName);
+        model.addAttribute("users",user);
+        return "trung/view";
+    }
+
+    @PostMapping("/edit")
+    public String editView(@ModelAttribute("users") User user,Model model){
+        userService.save(user);
+        model.addAttribute("users",user);
+        return "trung/view";
+    }
+
+    @GetMapping(value = "/editMember")
+    public String showMemberEdit(){
+        return "trung/editMember";
+    }
+    @GetMapping(value = "/editPass")
+    public String showMemberEditPass(){
+        return "trung/editPass";
+    }
+    @GetMapping(value = "/history")
+    public String showHistory(){
+        return "trung/history";
+    }
+    @GetMapping(value = "/register")
+    public String dashBoard(){
+        return "register";
     }
 }
